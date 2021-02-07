407d07f656ec1b421143bbf4bbc9dd343f6b92ed
Pranav More <13375461+moreiarty@users.noreply.github.com>
2021-02-07 22:55:24 +1100

    Make change to test deployment

--- PranavM.WxTechChallengeService.WebApi/Controllers/WeatherForecastController.cs
+++ PranavM.WxTechChallengeService.WebApi/Controllers/WeatherForecastController.cs
@@ -31,7 +31,7 @@ namespace PranavM.WxTechChallengeService.WebApi.Controllers
             {
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
+                Summary = Summaries[rng.Next(Summaries.Length)] + ", yikes!"
             })
             .ToArray();
         }
