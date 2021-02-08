62e2328e5392c8e0fad2e56ae0e5dfe72d4fc440
Pranav More <13375461+moreiarty@users.noreply.github.com>
2021-02-08 21:05:00 +1100

    Begin implementing sort endpoint

--- PranavM.WxTechChallengeService.WebApi/Startup.cs
+++ PranavM.WxTechChallengeService.WebApi/Startup.cs
@@ -10,7 +10,9 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using PranavM.WxTechChallengeService.WebApi.Constants;
 using PranavM.WxTechChallengeService.WebApi.Controllers.Implementation;
+using PranavM.WxTechChallengeService.WooliesRecruitmentService.Accessors.Configurations;
 
 namespace PranavM.WxTechChallengeService.WebApi
 {
@@ -28,6 +30,12 @@ namespace PranavM.WxTechChallengeService.WebApi
         {
             services.AddControllers();
             services.AddScoped<IWxTechChallengeController, WxTechChallengeControllerImplementation>();
+
+            var wooliesRecruitmentServiceConfigSection = Configuration.GetSection($"{ConfigConstants.WOOLES_RECRUITMENT_SERVICE_SECTION}");
+            services.Configure<WooliesRecruitmentServiceConfig>(wooliesRecruitmentServiceConfigSection);
+
+            var wooliesRecruitmentConfig = wooliesRecruitmentServiceConfigSection.Get<WooliesRecruitmentServiceConfig>();
+            Log.Information("Woolies Recruitment Service Base URL: {WooliesRecruitmentBaseUrl}", wooliesRecruitmentConfig?.BaseUrl);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
---
+++ PranavM.WxTechChallengeService.WooliesRecruitmentService.Accessors/Configurations/WooliesRecruitmentServiceConfig.cs
@@ -0,0 +1,9 @@
+namespace PranavM.WxTechChallengeService.WooliesRecruitmentService.Accessors.Configurations
+{
+    public class WooliesRecruitmentServiceConfig
+    {
+        public string BaseUrl { get; set; }
+
+        public string Token { get; set; }
+    }
+}
